d7ac530eda270d4918ae69063a0240e76fc4d95a
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-08-10 04:17:04 +0300

    Configured list filters and search capabilities

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -6,7 +6,7 @@ from .models import Book
 #admin.site.register(Book)
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    search_fields = ('title', 'author')
+    list_filters = ('title', 'author', 'publication_year')
+    search_capabilities = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
