a554209266032730b791beb2d218ef4533e45695
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-08-16 22:03:20 +0300

    modified models'return strings

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -7,7 +7,7 @@ class Author(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -21,11 +21,11 @@ class Library(models.Model):
     books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
-        return f"{self.name}"    
+        return self.name   
 
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarians')
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
