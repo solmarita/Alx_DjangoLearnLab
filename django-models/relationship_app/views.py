de83ecad167ec2229caa056fd111b697454029a1
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-08-17 00:54:19 +0300

    Added functional based list_books view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from .models import Author, Book, Library, Librarian
 
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all() #fetching all books from the database
+    context = {list_books:books} #creates a context dictionary with list of books
+    return render(request, 'books/list_books.html', context)
