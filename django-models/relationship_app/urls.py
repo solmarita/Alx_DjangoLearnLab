0d67a7af34e9cb5f25ae1c934a24c20e677bba8e
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-08-17 21:04:25 +0300

    Corrected spacing typos on the url file for template_name variables

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path("list_books/", list_books, name = "list_books"),
     path("library_detail/", LibraryDetailView.as_view(), name = "library_detail"),
     path("register/", views.register, name = "register"),
-    path("login/", LoginView.as_view(template_name ="login.html"), name = "login"),
-    path("logout/", LogoutView.as_view(template_name="logout.html"), name = "logout"),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
     path("", index, name = "index")
 
 ]
