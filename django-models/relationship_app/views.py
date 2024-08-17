0d86850e1962e82b34a5ace904fbd92a4d38430a
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-08-17 17:52:35 +0300

    Correctly imported the models in the view.py file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,17 +1,20 @@
 from typing import Any
 from django.shortcuts import render
 #from django.http import HttpResponse
-from django.views.generic import TemplateView, ListView, DetailView
-from .models import Author, Book, Library, Librarian
+#from django.views.generic import DetailView
+#from .models import Author, Book, Library, Librarian
 
 # Create your views here.
+from .models import Book
 def list_books(request):
     books = Book.objects.all() #fetching all books from the database
     context = {'list_books':books} #creates a context dictionary with list of books
     return render(request, 'relationship_app/list_books.html', context)
 
 
+from django.views.generic import DetailView
 from .models import Library
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
