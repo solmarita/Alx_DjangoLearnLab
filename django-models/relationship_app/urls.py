9009f195e5fabd0692b59a8aefd183cd38c0d244
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-08-17 18:06:30 +0300

    Corrected the apps URLs configuration

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path("list_books/", views.list_books, name = "list_books"),
-    path("library_detail/", views.LibraryDetailView.as_view(), name = "library_detail")
+    path("list_books/", list_books, name = "list_books"),
+    path("library_detail/", LibraryDetailView.as_view(), name = "library_detail")
 
 ]
