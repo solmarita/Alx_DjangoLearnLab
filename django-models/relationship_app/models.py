23135b18bac5c28853d94aca8a6df20019d1bcbf
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-08-18 00:00:15 +0300

    Extended User Model with a UserProfile

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -29,3 +30,17 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+    
+#Extending User Model with a UserProfile
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    Role_Choices =(
+        ('Admin','Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    
+    )
+
+role = models.CharField(max_length=50,  Choices = 'Role_Choices')
+userprofile = models.TextField()
