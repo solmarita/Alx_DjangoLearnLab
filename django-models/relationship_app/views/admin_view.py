56557a69e9828bcd18d91b04e6722002781cbaf3
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-08-18 02:23:03 +0300

    Removed redundant code in role based views creations

--- django-models/relationship_app/views/admin_view.py
+++ django-models/relationship_app/views/admin_view.py
@@ -1,10 +1,13 @@
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
 @login_required
+@user_passes_test(is_admin)
 def admin_view(request):
-    if request.user.userprofile.role != 'Admin':
-        return HttpResponseForbidden("You do not have permission to view this page.")
     return render(request, 'relationship_app/admin_view.html')
