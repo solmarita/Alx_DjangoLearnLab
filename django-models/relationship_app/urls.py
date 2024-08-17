4422b1e366b110e19a21a2b5e930393e706643b2
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-08-17 19:48:53 +0300

    Created register, login and logout user authentication

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, register, index, LoginView, LogoutView
 
 urlpatterns = [
     path("list_books/", list_books, name = "list_books"),
-    path("library_detail/", LibraryDetailView.as_view(), name = "library_detail")
+    path("library_detail/", LibraryDetailView.as_view(), name = "library_detail"),
+    path("register", register, name = "register"),
+    path("login", LoginView.as_view(), name = "login"),
+    path("logout", LogoutView.as_view(next_page="/"), name = "logout"),
+    path("", index, name = "index")
 
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,6 @@
 from typing import Any
-from django.shortcuts import render
-#from django.http import HttpResponse
-#from django.views.generic import DetailView
-#from .models import Author, Book, Library, Librarian
+from django.shortcuts import render,redirect
+
 
 # Create your views here.
 from .models import Book
@@ -24,4 +22,32 @@ class LibraryDetailView(DetailView):
         context['books_list'] = library.get_books_list()
         return context
     
+
+#Setup User Authentication Views
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
+
+#User login view
+class LoginView(LoginView):
+    template_name = "login.html"
+
+#user logout view
+class LogoutView(LogoutView):
+    template_name = "logout.html"
+
+#homepage view
+def index(request):
+    return render(request, "index.html")
+
 
