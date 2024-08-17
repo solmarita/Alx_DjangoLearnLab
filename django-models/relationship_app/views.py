05eda83659c3ce0c98b8cfadd80387973d50e7f6
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-08-17 16:29:03 +0300

    Redefined the path for the class-based view to relationship_app/library_detail.html

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -12,7 +12,7 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'libraries/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         library = self.get_object()
