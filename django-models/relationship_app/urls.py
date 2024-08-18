04f1ba9a10ea8e7f7339c5e8520f67eb661e2dbe
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-08-18 10:29:13 +0300

    Corrected urls paths for admin, member and librarian views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
     path("", index, name="index"),
-    path('admin/', views.admin_view, name='admin_view'),
-    path('librarian/', views.librarian_view, name='librarian_view'),
-    path('member/', views.member_view, name='member_view'),
+    path('admin_view/', views.admin_view, name='admin_view'),
+    path('librarian_view/', views.librarian_view, name='librarian_view'),
+    path('member_view/', views.member_view, name='member_view'),
 
 ]
