be38446031cd94df09cad331a685644a3d636112
Solomon Marita <officialmokua@gmail.com>
2024-09-01 21:07:11 +0300

    Answered quesiton 2

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
-from .views import BookListCreate
+from django.urls import path, include #Added 'include' for question 2
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet#,BookListCreate # Uncomment '#,BookListCreate' to answer question 1
 
+# Uncomment this to answer question 1
+# urlpatterns = [
+#     path('books/', BookListCreate.as_view(), name='book-list-create'),
+# ]
+
+# Answer to question 2
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('books/', BookListCreate.as_view(), name='book-list-create'),
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import viewsets # Added 'viewsets' for question 2
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreate(generics.ListAPIView):
+# Uncomment this to answer question 1
+# class BookListCreate(generics.ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+
+#Answer to question 2
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing Book instances.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
 
