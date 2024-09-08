e76bbeb58def8e6273b91161794563a9944a40fd
Solomon Marita <officialmokua@gmail.com>
2024-09-08 22:09:52 +0300

    Added unit tests

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,56 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Book, Author
+
+class BookAPITests(APITestCase):
+    def setUp(self):
+        # Create test data
+        self.author = Author.objects.create(name="Author Name")
+        self.book = Book.objects.create(
+            title="Book Title",
+            publication_year=2022,
+            author=self.author
+        )
+        self.book_url = reverse('book-list')  # Ensure your URL names match
+        self.book_detail_url = reverse('book-detail', args=[self.book.id])
+
+    def test_create_book(self):
+        data = {
+            'title': 'New Book',
+            'publication_year': 2023,
+            'author': self.author.id
+        }
+        response = self.client.post(self.book_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 2)
+        self.assertEqual(Book.objects.get(id=response.data['id']).title, 'New Book')
+
+    def test_update_book(self):
+        data = {
+            'title': 'Updated Title',
+            'publication_year': 2024
+        }
+        response = self.client.put(self.book_detail_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.get(id=self.book.id).title, 'Updated Title')
+
+    def test_delete_book(self):
+        response = self.client.delete(self.book_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
+
+    def test_filter_books(self):
+        response = self.client.get(self.book_url, {'title': 'Book Title'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+
+    def test_search_books(self):
+        response = self.client.get(self.book_url, {'search': 'Book Title'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+
+    def test_ordering_books(self):
+        response = self.client.get(self.book_url, {'ordering': 'publication_year'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'][0]['publication_year'], 2022)
