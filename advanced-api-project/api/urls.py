f0baf1f7b7b8b467f6a8388cf35c6f23671ac20c
Solomon Marita <officialmokua@gmail.com>
2024-09-08 21:48:38 +0300

    Added api url in the correct urls file

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(["api.urls"]),
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
