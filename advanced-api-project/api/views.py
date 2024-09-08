58f98ae59767dbd651f9afede9dd106272b3f7f7
Solomon Marita <officialmokua@gmail.com>
2024-09-08 21:36:31 +0300

    Imported missing definitions from the rest_framework.permissions module

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
