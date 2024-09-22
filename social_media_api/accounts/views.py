3bc9bb1e696ba25e2c8d16be8a5b84f9d88b4057
Solomon Marita <officialmokua@gmail.com>
2024-09-22 11:46:56 +0300

    Fixed views to add ALX specific code formats

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,12 +1,12 @@
 from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
+import rest_framework.permissions as permissions
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from accounts.models import CustomUser
 
 # Follow a user
 class FollowUserView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         target_user = get_object_or_404(CustomUser, id=user_id)
@@ -17,10 +17,10 @@ class FollowUserView(generics.GenericAPIView):
 
 # Unfollow a user
 class UnfollowUserView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        target_user = get_object_or_404(CustomUser, id=user_id)
+        target_user = CustomUser.objects.all()
         if target_user != request.user:
             request.user.following.remove(target_user)
             return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
