41c1c78eae19e3b239322a2620c6d51280d9fc6c
Solomon Marita <officialmokua@gmail.com>
2024-09-22 11:51:29 +0300

    Replaced single quotes with double quotes in accounts/urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('register/', RegisterUserView.as_view, name='register'),
     path('login/', LoginUserView.as_view, name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path("follow/<int:user_id>/", views.follow_user, name='follow_user'),
+    path("unfollow/<int:user_id>/", views.unfollow_user, name='unfollow_user'),
 ]
