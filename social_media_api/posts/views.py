d57ab4105e40befb53edc06a1f847f5b65e6833e
Solomon Marita <officialmokua@gmail.com>
2024-09-22 11:48:31 +0300

    posts/views.py fixed to include specific code ALX checker requires

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,82 +1,28 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
+from rest_framework.response import Response
+from posts.models import Post
+from accounts.models import CustomUser
 
-# Create your views here.
-#Views for posts
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['title', 'content']
-    search_fields = ['title', 'content']
+# Feed view to show posts from followed users
+class FeedView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
 
-    def create(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+    def get(self, request):
+        # Get all users the current user is following
+        following_users = request.user.following.all()
 
-    def create_post(self, request, *args, **kwargs):
-        queryset = Post.objects.all()
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
+        # Filter posts by authors who are in the following list, ordered by creation date
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
 
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-        
-    def list(self, request, *args, **kwargs):
-        quueryset = Post.objects.all()
-        serializer = PostSerializer(quueryset, many=True)
-        return Response(serializer.data)
-    
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
-    
-    def update(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
-    
-    def destroy(self, request, *args, **kwargs):
-        return super().destroy(request, *args, **kwargs)
+        # Prepare the post data to return
+        post_data = [
+            {
+                'author': post.author.username,
+                'content': post.content,
+                'created_at': post.created_at
+            }
+            for post in posts
+        ]
 
-#comment viewsets
-class CommentViewSet(viewsets.ModelViewSet):
-    def create(self, request, *args, **kwargs):
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-
-    def list(self, request, *args, **kwargs):
-        quueryset = Comment.objects.all()
-        serializer = CommentSerializer(quueryset, many=True)
-        return Response(serializer.data)
-    
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
-    
-    def update(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
-    
-    def destroy(self, request, *args, **kwargs):
-        return super().destroy(request, *args, **kwargs)
-    
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def feed(request):
-    followed_users = request.user.following.all()
-    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
-    post_data = [{'author': post.author.username, 'content': post.content, 'created_at': post.created_at} for post in posts]
-    return Response(post_data)
+        return Response(post_data)
